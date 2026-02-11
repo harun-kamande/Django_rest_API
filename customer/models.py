f975caeebac10fc3c1b9dae44d30df70074302ef
harun-kamande <kamandeharun99@gmail.com>
2026-02-12 02:47:18 +0300

    others apps were added

--- customer/models.py
+++ customer/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
-class User(AbstractUser):
+class Customers(AbstractUser):
     age = models.IntegerField(null=True, blank=True)
     gender = models.CharField(max_length=10, null=True, blank=True)
     country = models.CharField(max_length=50, null=True, blank=True)
+
+    # def get_customers(self):
+    #     return User.objects.filter(user=self)
--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+from .models import Customers
+from .serializers import CustomerSerializer
+
+
 # Create your views here.
+class CustomerView(APIView):
+    def get(self, request):
+        get_customers = Customers.objects.all()
+        serializer = CustomerSerializer(get_customers, many=True)
+        return Response({"message": "Customer View GET method", "data": serializer.data})
+    
+    def post(self, request):
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Customer View POST method", "data": serializer.data})
+        return Response(serializer.errors, status=400)
