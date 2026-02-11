a9cdf15edb6b9341eceb50556513187c0e1f9322
harun-kamande <kamandeharun99@gmail.com>
2026-02-12 02:47:18 +0300

    others apps were added

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from .views import (
-    UserListView,
-    CustomerView,
-    CustomerDeleteView,
-    CustomerUpdateView,
-)
+from customer.views import CustomerView
 
 urlpatterns = [
-    path('users/', UserListView.as_view(), name='user-list'),
-    path('', UserListView.as_view(), name='user-list-default'),
+    path('users/', CustomerView.as_view(), name='user-list'),
+    path('', CustomerView.as_view(), name='user-list-default'),
     path('customers/', CustomerView.as_view(), name='customer-list-create'),
-    path('delete/', CustomerDeleteView.as_view(), name='customer-delete'),
-    path('update/', CustomerUpdateView.as_view(), name='customer-update'),
+    path('delete/', CustomerView.as_view(), name='customer-delete'),
+    path('update/', CustomerView.as_view(), name='customer-update'),
 ]
