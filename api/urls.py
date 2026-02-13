5f0c4f5f7bdc84967eeb0101a1efc873e1c15ba7
harun-kamande <kamandeharun99@gmail.com>
2026-02-13 23:50:18 +0300

    changes

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from customer.views import CustomerView
+from rest_framework.authtoken.views import obtain_auth_token
+
+
 
 urlpatterns = [
+    path
     path('users/', CustomerView.as_view(), name='user-list'),
     path('', CustomerView.as_view(), name='user-list-default'),
     path('customers/', CustomerView.as_view(), name='customer-list-create'),
