c9a86cba869823826ce7b35dd93456df801c41b5
harun-kamande <kamandeharun99@gmail.com>
2026-02-16 02:35:41 +0300

    changes

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
 from customer.views import CustomerView
 from rest_framework.authtoken.views import obtain_auth_token
+from product.views import ProductView
 
+
+
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)   
 
 
 urlpatterns = [
-    path
+
+    path("token/", obtain_auth_token, name="api_token_auth"),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/', CustomerView.as_view(), name='user-list'),
+    path('products/', ProductView.as_view(), name='product-list-create'),
     path('', CustomerView.as_view(), name='user-list-default'),
     path('customers/', CustomerView.as_view(), name='customer-list-create'),
     path('delete/', CustomerView.as_view(), name='customer-delete'),
