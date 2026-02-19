4605112b5c20015f03cb71401d3af484fb9fad4b
harun-kamande <kamandeharun99@gmail.com>
2026-02-19 21:10:48 +0300

    changes

--- simple_api/settings.py
+++ simple_api/settings.py
@@ -147,6 +147,8 @@ REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
         'user': '5/day',
-    }
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
 }
 
